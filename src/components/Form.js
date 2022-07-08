f13cdbb9ef3ffeda65563d33d9cff82520668ee2
Ronald Jean-Francois <105710107+ronthetech@users.noreply.github.com>
2022-07-08 18:10:41 -0400

    added empty field messages

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 const Form = () => {
 	const [values, setValues] = useState({
 		name: "",
+		number: "",
 		email: "",
 		message: "",
 	});
@@ -11,6 +12,9 @@ const Form = () => {
 
 	const handleNameInputChange = (e) => {
 		setValues({ ...values, name: e.target.value });
+	};
+	const handleNumberInputChange = (e) => {
+		setValues({ ...values, number: e.target.value });
 	};
 	const handleEmailInputChange = (e) => {
 		setValues({ ...values, email: e.target.value });
@@ -32,8 +36,18 @@ const Form = () => {
 			<form className='form' onSubmit={handleFormSubmit}>
 				<label htmlFor='formName'>Name</label>
 				<input className='form__name' id='formName' name='formName' type='text' value={values.name} onChange={handleNameInputChange} placeholder='Name' />
+				{submitted && !values.name ? <span className='empty--field'>Please enter a name</span> : null}
 				<label htmlFor='formNumber'>Phone Number</label>
-				<input className='form__number' id='formNumber' type='tel' placeholder='Phone Number' />
+				<input
+					className='form__number'
+					id='formNumber'
+					type='tel'
+					value={values.number}
+					onChange={handleNumberInputChange}
+					pattern='[0-9]{3}-[0-9]{2}-[0-9]{3}'
+					placeholder='Phone Number'
+				/>
+				{submitted && !values.number ? <span className='empty--field'>Please enter a phone number</span> : null}
 				<label htmlFor='formEmail'>Email Address</label>
 				<input
 					className='form__email'
@@ -44,6 +58,7 @@ const Form = () => {
 					onChange={handleEmailInputChange}
 					placeholder='Email Address'
 				/>
+				{submitted && !values.email ? <span className='empty--field'>Please enter an email address</span> : null}
 				<label htmlFor='formText'>Send A Message To Let Us Know What Service You Need!</label>
 				<textarea
 					value={values.message}
@@ -54,6 +69,7 @@ const Form = () => {
 					placeholder='Message'
 					rows='4'
 				/>
+				{submitted && !values.message ? <span className='empty--field'>Please enter a message</span> : null}
 				<button className='form__btn'>Submit</button>
 			</form>
 		</div>
