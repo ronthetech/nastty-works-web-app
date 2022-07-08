687cf08ff58008e4eb454c8d90bc36d55ab7d321
Ronald Jean-Francois <105710107+ronthetech@users.noreply.github.com>
2022-07-08 12:31:50 -0400

    styled contact form

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,11 +2,15 @@ const Form = () => {
 	return (
 		<div className='form__container'>
 			<form className='form'>
-				<input className='form__name' id='formName' type='text' placeholder='Name' /*value='name'*/ />
-				<input className='formNumber' id='formNumber' type='number' placeholder='Phone Number' />
-				<input className='form__email' id='formEmail' type='email' placeholder='Email Address' />
-				<input className='form__text' id='formText' type='textarea' placeholder='Message' />
-				<button></button>
+				<label htmlFor='formName'>Name</label>
+				<input className='form__name' id='formName' name='formName' type='text' placeholder='Name' />
+				<label htmlFor='formNumber'>Phone Number</label>
+				<input className='form__number' id='formNumber' type='tel' placeholder='Phone Number' />
+				<label htmlFor='formEmail'>Email Address</label>
+				<input className='form__email' id='formEmail' name='formEmail' type='email' placeholder='Email Address' />
+				<label htmlFor='formText'>Send A Message To Let Us Know What Service You Need!</label>
+				<textarea className='form__text' name='formText' id='formText' placeholder='Message' rows='4' />
+				<button className='form__btn'>Submit</button>
 			</form>
 		</div>
 	);
