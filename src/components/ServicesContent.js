a3b63a2d90d0d9d272074d08d85231d3b5853e2d
Ronald Jean-Francois <105710107+ronthetech@users.noreply.github.com>
2022-07-08 22:31:54 -0400

    added servicescontent

--- src/components/ServicesContent.js
+++ src/components/ServicesContent.js
@@ -1,5 +1,26 @@
 const ServicesContent = () => {
-	return <div></div>;
+	return (
+		<div className='services__container'>
+			<h3 className='services__heading'>Some of the services we provide:</h3>
+			<ul className='services__list'>
+				<li>brake jobs</li>
+				<li>camber adjustment</li>
+				<li>coilovers</li>
+				<li>control arms</li>
+				<li>tie rods</li>
+				<li>serpentine belts</li>
+				<li>expansion/coolant tanks</li>
+				<li>cooling system refresh</li>
+				<li>motor mounts</li>
+				<li>fender rolling</li>
+				<li>full adjustment</li>
+				<li>and more!</li>
+			</ul>
+			<h4 className='services__message'>
+				If we cant handle your specific case, we know someone who can! Get in touch with us on the Contact page for more info
+			</h4>
+		</div>
+	);
 };
 
 export default ServicesContent;
