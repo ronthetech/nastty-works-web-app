1ca9966e42efe365fa1304335309eccab108af9c
Ronald Jean-Francois <105710107+ronthetech@users.noreply.github.com>
2022-07-11 11:11:55 -0400

    added section for photo gallery

--- src/components/MediaContent.js
+++ src/components/MediaContent.js
@@ -15,6 +15,25 @@ const MediaContent = () => {
 			<div className='media__item'>
 				<InstagramEmbed url='https://www.instagram.com/reel/CdqMfRDls4O/' width={328} />
 			</div>
+			<div className='media__gallery'>
+				<div className='gallery__item'>
+					<img src='' alt='' srcset='' className='gallery__img' />
+				</div>
+				<div className='gallery__item'>
+					<img src='' alt='' srcset='' className='gallery__img' />
+				</div>
+			</div>
+			{/* <div className='media__archive'>
+			//link to archive with images matching year or topic
+				<div className='archive__item'>
+					<img src='' alt='' srcset='' className='archive__img' />
+					<p>Best Of 2021</p>
+				</div>
+				<div className='archive__item'>
+					<img src='' alt='' srcset='' className='archive__img' />
+					<p>Best Of Summer</p>
+				</div>
+			</div> */}
 		</div>
 	);
 };
